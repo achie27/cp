43175a4c680675f3eca89f8fc7b69d7bccf5e552
achie27 <architmathur2011@gmail.com>
2021-02-28 11:10:07 +0530

    just a bit of formatting for you

--- min_swaps.go
+++ min_swaps.go
@@ -9,35 +9,33 @@ func main() {
 }
 
 /**
- * @input A : Integer array
- * 
- * @Output Integer
- */
- func solve(A []int )  (int) {
-    cur_elem_pos := make([]int, len(A))
-    for i := 0; i < len(A); i++ {
-        cur_elem_pos[A[i]] = i
-    }
-    
-    swaps := 0
-    for i := 0; i < len(A); i++ {
+* @input A : Integer array
+* 
+* @Output Integer
+*/
+func solve(A []int )  (int) {
+	cur_elem_pos := make([]int, len(A))
+	for i := 0; i < len(A); i++ {
+		cur_elem_pos[A[i]] = i
+	}
+
+	swaps := 0
+	for i := 0; i < len(A); i++ {
 		fmt.Println(i, cur_elem_pos, A)
 
-        if A[i] != i {
-            swaps++
-            
-            arr_pos_i := cur_elem_pos[i]
+		if A[i] != i {
+			swaps++
 
-            cur_elem_pos[i] = i
-            cur_elem_pos[A[i]] = arr_pos_i
-            
-            A[arr_pos_i] = A[i]
-            A[i] = i
+			arr_pos_i := cur_elem_pos[i]
 
-        }
-    }
+			cur_elem_pos[i] = i
+			cur_elem_pos[A[i]] = arr_pos_i
+
+			A[arr_pos_i] = A[i]
+			A[i] = i
+		}
+	}
 
 	fmt.Println("last", cur_elem_pos, A)
-    
-    return swaps
+	return swaps
 }
