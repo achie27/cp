39dc7881186920fd365635510063e4c8d687038d
achie27 <architmathur2011@gmail.com>
2021-03-13 13:43:15 +0530

    took longer than usual

---
+++ math/rectangle_overlap_area.py
@@ -0,0 +1,16 @@
+class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @param C : integer
+    # @param D : integer
+    # @param E : integer
+    # @param F : integer
+    # @param G : integer
+    # @param H : integer
+    # @return an integer
+    def solve(self, A, B, C, D, E, F, G, H):
+        
+        if A >= G or E >= C: return 0
+        if B >= H or F >= D: return 0
+        
+        return (min(G, C) - max(A, E)) * (min(D, H) - max(B, F))
